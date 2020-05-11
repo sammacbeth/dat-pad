5594e1f7e761b4d80a06e70f79de96ce48068feb
Sam Macbeth <sam@cliqz.com>
2020-05-11 15:39:26 +0200

    Update writer and peer counts

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Corestore = require('corestore');
-const RandomAccess = require('random-access-idb');
-const Discovery = require('@geut/discovery-swarm-webrtc');
-const pump = require('pump');
+const Corestore = require("corestore");
+const RandomAccess = require("random-access-idb");
+const Discovery = require("@geut/discovery-swarm-webrtc");
+const pump = require("pump");
 
-const CodeMirror = require('codemirror');
-const { CodemirrorBinding } = require('y-codemirror/dist/y-codemirror.cjs');
-require('codemirror/mode/markdown/markdown');
+const CodeMirror = require("codemirror");
+const { CodemirrorBinding } = require("y-codemirror/dist/y-codemirror.cjs");
+require("codemirror/mode/markdown/markdown");
 
-const MarkdownIt = require('markdown-it');
+const MarkdownIt = require("markdown-it");
 
-const { DatYDoc } = require("@sammacbeth/dat-shared-doc")
+const { DatYDoc } = require("@sammacbeth/dat-shared-doc");
 
 function randomName() {
   return `store_${Math.ceil(Math.random() * 1e8).toString(10)}`;
@@ -36,7 +36,7 @@ function storeName() {
     announceFeeds: true,
     awareness: true,
     bootstrapEnabled: true,
-    policy: "GRANT_ALL_ADMIN"
+    policy: "GRANT_ALL_ADMIN",
   };
   const ydoc = address
     ? await DatYDoc.load(address, store, opts)
@@ -51,11 +51,15 @@ function storeName() {
   }
   const addressBuf = Buffer.from(address, "hex");
 
+  let peers = 0;
   sw.on("connection", (conn, info) => {
     const stream = store.replicate(info.initiator, { live: true });
+    peers += 1;
     pump(stream, conn, stream, (err) => {
-      console.log("stream end/error", err);
+      peers -= 1;
+      updatePeerCount(peers);
     });
+    updatePeerCount(peers);
   });
   sw.join(addressBuf);
 
@@ -63,35 +67,55 @@ function storeName() {
   const type = ydoc.doc.getText("codemirror");
 
   function setupEditor() {
-    const div = document.getElementById('editor') || document.createElement('div')
-    div.setAttribute('id', 'editor');
-    div.setAttribute('class', 'column');
-    document.getElementById('content').insertBefore(div, document.getElementById('doc'));
+    const div =
+      document.getElementById("editor") || document.createElement("div");
+    div.setAttribute("id", "editor");
+    div.setAttribute("class", "column");
+    document
+      .getElementById("content")
+      .insertBefore(div, document.getElementById("doc"));
     const editor = CodeMirror(div, {
-      mode: 'markdown',
+      mode: "markdown",
       lineNumbers: true,
       lineWrapping: true,
       spellcheck: true,
       viewportMargin: Infinity,
-    })
-    const binding = new CodemirrorBinding(type, editor, ydoc.awareness.awareness);
+    });
+    const binding = new CodemirrorBinding(
+      type,
+      editor,
+      ydoc.awareness.awareness
+    );
     window.binding = binding;
   }
-  
-  if (ydoc.writable){
+
+  if (ydoc.writable) {
     setupEditor();
   } else {
-    ydoc.multicore.once('writer', () => {
-      setupEditor()
-    })
+    ydoc.multicore.once("writer", () => {
+      setupEditor();
+    });
   }
-  
 
   const md = new MarkdownIt();
-  const docContents = document.getElementById('doc');
+  const docContents = document.getElementById("doc");
   type.observe(() => {
-    docContents.innerHTML = md.render(type.toJSON())
-  })
+    docContents.innerHTML = md.render(type.toJSON());
+  });
+
+  ydoc.multicore.metadata.doc.on("update", () => {
+    updateWriterCount();
+  });
+  updateWriterCount();
+
+  function updateWriterCount() {
+    const writers = [...ydoc.multicore.metadata.getFeeds("ydoc").keys()].length;
+    document.getElementById("writer-count").innerText = `${writers}`;
+  }
 
   window.ydoc = ydoc;
 })();
+
+function updatePeerCount(peers) {
+  document.getElementById("peer-count").innerText = `${peers}`;
+}
